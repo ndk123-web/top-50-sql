785aba3bc62d85c048a3842d25cf94c0c1e1421c
Navnath Kadam <navnathkadam284@gmail.com>
2026-08-25 12:37:28 +0530

    Time: 1736 ms (26.72%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,33 @@
+WITH x AS (
+    SELECT 
+        *,
+        (
+            CASE
+                WHEN a.income < 20000 THEN "Low Salary"
+                WHEN a.income > 50000 THEN "High Salary"
+                ELSE "Average Salary" 
+            END
+        ) as category 
+    FROM 
+        Accounts as a 
+),
+    u AS (
+        SELECT 'Low Salary' AS category
+        UNION ALL
+        SELECT 'Average Salary'
+        UNION ALL
+        SELECT 'High Salary' 
+    )
+
+SELECT 
+    u.category,
+    (
+        CASE
+            WHEN x.category IS NULL THEN 0
+            ELSE COUNT(*)
+        END
+    ) as accounts_count 
+FROM u
+LEFT JOIN x
+ON x.category = u.category
+GROUP BY u.category;
