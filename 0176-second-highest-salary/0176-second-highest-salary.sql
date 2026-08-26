3f295a3ca71f4ca9657ce874db13c0b6251e481b
Navnath Kadam <navnathkadam284@gmail.com>
2026-08-26 12:26:08 +0530

    Time: 275 ms (82.42%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,19 +1,6 @@
-WITH CTE AS (
-    SELECT 
-        *,
-        DENSE_RANK() OVER (
-            ORDER BY Employee.salary
-        ) as rnk 
-    FROM 
-        Employee
+SELECT MAX(salary) as SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM Employee
 )
-
-SELECT 
-    (
-        CASE
-            WHEN COUNT(*) = 0 THEN NULL
-            ELSE CTE.salary
-        END
-    ) as SecondHighestSalary
-FROM CTE 
-WHERE CTE.rnk = 2;
