6e808d8568993cde9f007ecdee241fa17fdc70da
Navnath Kadam <navnathkadam284@gmail.com>
2026-08-27 17:28:12 +0530

    Time: 569 ms (82.72%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -3,34 +3,14 @@ WITH x AS (
         *,
         COUNT(*) OVER (
             PARTITION BY tiv_2015
-        ) as cnt 
+        ) as cnt,
+        COUNT(*) OVER (
+            PARTITION BY lon, lat
+        ) as loc_cnt
     FROM
         Insurance 
-),
+)
 
-    y AS (
-        SELECT 
-            *,
-            COUNT(*) OVER (
-                PARTITION BY lon, lat
-            ) as loc_cnt
-        FROM
-            Insurance
-    ),
-
-    z AS (
-        SELECT *
-        FROM y
-        WHERE y.loc_cnt = 1 
-    ),
-
-    u AS (
-        SELECT *
-        FROM x
-        WHERE x.cnt > 1
-    )
-
-SELECT ROUND(SUM(u.tiv_2016), 2) as tiv_2016
-FROM z
-JOIN u
-ON z.pid = u.pid;
+SELECT ROUND(SUM(x.tiv_2016), 2) as tiv_2016
+FROM x
+WHERE x.cnt > 1 AND x.loc_cnt = 1;
